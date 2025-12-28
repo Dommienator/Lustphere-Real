6ee916ab1e5f9bc66abab4a221e3c9cf7e18d224
Dominic Kabingu <your-email@example.com>
2025-12-28 23:34:44 +0300

    Backend+Frontend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_TO_KSH = 23;
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 export const PROFILES_PER_PAGE = 20;
 
 export const PRICING_TIERS = [
